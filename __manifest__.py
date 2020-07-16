664a33186d38b84572360822337e3c71b9b9fa9b
pafnow <pafnow@hotmail.com>
2020-07-16 11:07:49 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Pafnow
+{
+    'name': 'PostgreSQL exception handler',
+    'version': '1.0',
+    'depends': ['web'],
+    'author': 'Pafnow',
+    'category': 'Technical Settings',
+    'description': 'Odoo client exception handler for PostgreSQL database exception raised by triggers',
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ static/src/js/exception_handler.js
@@ -0,0 +1,19 @@
+/* Copyright 2020 Pafnow */
+odoo.define('web_db_exception_handler.exception_handler', function (require) {
+    'use strict';
+
+    var core = require('web.core');
+
+    var DBExceptionHandler = core.Class.extend({
+        init: function(parent, error) {
+            this.error = error;
+        },
+        display: function() {
+            alert('Damien est trop fort!');
+        }
+    });
+
+    core.crash_registry.add('psycopg2.errors.RaiseException', DBExceptionHandler);
+
+    return DBExceptionHandler;
+});
